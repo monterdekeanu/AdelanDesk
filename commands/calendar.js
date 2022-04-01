39aee00352a7e070fc57e8edae73349f2e9bba29
Keanu Anthony Monterde <75210937+monterdekeanu@users.noreply.github.com>
2022-04-01 13:11:13 +0000

    - Added Registration JS - Added Calendar.json for User pseudo-database

--- commands/calendar.js
+++ commands/calendar.js
@@ -10,39 +10,53 @@ module.exports = {
   callback: async ({ interaction, message, text }) => {
     const embed = new MessageEmbed().setColor('GREEN')
     // use the sync function parseFile() to parse this ics file
-    const events = ical.sync.parseFile('feed.ics');
+   
     // loop through events and log them
-    const url = 'https://adelantelms.usjr.edu.ph/d2l/le/calendar/feed/user/feed.ics?token=aae2xhrhc1fx58ug283a';
-  
-    https.get(url,(res) => {
-        // Image will be stored at this path
-        const path = `feed.ics`; 
-        const filePath = fs.createWriteStream(path);
-        res.pipe(filePath);
-        filePath.on('finish',() => {
-            filePath.close();
-            console.log('Download Completed'); 
-        })
+    var calendarDb = require('../db/calendar.json');
+    var flag = 0;
+    var status = 0;
+    calendarDb.some(block =>{
+      if(block[0]==message.author.username + "#" + message.author.discriminator){
+        const url = block[1];
+        flag=1
+        console.log("Success")
+        https.get(url, (res) => {
+      const path = `feed.ics`;
+      const filePath = fs.createWriteStream(path);
+      res.pipe(filePath);
+      filePath.on('finish', () => {
+        filePath.close();
+        console.log('Download Completed');
+      })
     })
-    var totalTask = 0;
-    setTimeout(()=>{
-      var today = new Date()
-       for (const event of Object.values(events)) {
-         var localDay = event.end.toLocaleDateString("en-SG",{ day: 'numeric'})
-         var localYear = event.end.toLocaleDateString("en-SG",{ year: 'numeric'})
-         var localMonth = event.end.toLocaleDateString("en-SG",{ month: 'numeric'})
-
-         
-      if((event.summary.toString().slice((event.summary.toString().length-3,event.summary.toString().length) == "Due") || (event.summary.toString().slice(event.summary.toString().length-4,event.summary.toString().length) == "Ends")) && (localYear >= today.getYear()&&localMonth == today.getMonth()+1 && localDay >= today.getDate() || localYear >= today.getYear()&&localMonth > today.getMonth()+1) && (today.getTime() < event.end.getTime()) ){
-        totalTask++
-        event.end.getTime();
-        embed.addField(months[event.end.getMonth()] + " " + event.end.getDate() + " | " + event.end.toLocaleTimeString("en-SG",{ hour: '2-digit', minute:'2-digit',timeZone: "Asia/Singapore" }),event.location+ ` \n`+" "+ event.summary,false)
-      }
-      
-    };
-    embed.setTitle(`USJ-R BS Computer Engineering | CPE1_1 | Total Tasks: ${totalTask}`)
-    message.channel.send({ embeds: [embed] })
-},2500)
     
+    var totalTask = 0;
+    setTimeout(() => { 
+      const events = ical.sync.parseFile('feed.ics');
+      var today = new Date()
+      for (const event of Object.values(events)) {
+        var localDay = event.end.toLocaleDateString("en-SG", { day: 'numeric' })
+        var localYear = event.end.toLocaleDateString("en-SG", { year: 'numeric' })
+        var localMonth = event.end.toLocaleDateString("en-SG", { month: 'numeric' })
+        if ((event.summary.toString().slice((event.summary.toString().length - 3, event.summary.toString().length) == "Due") || (event.summary.toString().slice(event.summary.toString().length - 4, event.summary.toString().length) == "Ends")) && (localYear >= today.getYear() && localMonth == today.getMonth() + 1 && localDay >= today.getDate() || localYear >= today.getYear() && localMonth > today.getMonth() + 1) && (today.getTime() < event.end.getTime())) {
+          totalTask++
+          event.end.getTime();
+          embed.addField(months[event.end.getMonth()] + " " + event.end.getDate() + " | " + event.end.toLocaleTimeString("en-SG", { hour: '2-digit', minute: '2-digit', timeZone: "Asia/Singapore" }), event.location + ` \n` + " " + event.summary, false)
+        }
+
+      };
+      embed.setTitle(`USJ-R Schedule for: ${message.author.username} | Total Tasks: ${totalTask}`)
+      message.channel.send({ embeds: [embed] })
+    }, 3000)
+        
+      }
+    })
+    if(flag == 0){
+      message.channel.send(message.author.username + " is not registered");
+    }
+    
+
+    
+
   },
 }
